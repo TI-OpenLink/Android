10148c3a60c067f76b15b6bdf53fbcbeaabe9426
Ido Reis <idor@ti.com>
2011-11-01 13:46:01 +0200

    nvs: default nvs file modified to wl12xx-nvs.bin

--- WCS_Tools/patches/wlan/nlcp/android/packages/hardware/wlan/fw/Android.mk
+++ WCS_Tools/patches/wlan/nlcp/android/packages/hardware/wlan/fw/Android.mk
@@ -2,7 +2,7 @@ local_target_dir := $(TARGET_OUT)/etc/firmware/ti-connectivity
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl1271-nvs.bin
+LOCAL_MODULE := wl12xx-nvs.bin
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)

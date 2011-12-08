8cede41841c011af942fd08fc2a1ce0a18b38103
Ido Reis <idor@ti.com>
2011-12-08 10:17:17 +0200

    updated Android.mk for firmwares
    Signed-off-by: Ido Reis <idor@ti.com>

--- WCS_Tools/patches/wlan/nlcp/android/packages/hardware/wlan/fw/Android.mk
+++ WCS_Tools/patches/wlan/nlcp/android/packages/hardware/wlan/fw/Android.mk
@@ -10,7 +10,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl1271-fw-multirole-plt.bin
+LOCAL_MODULE := wl128x-fw-mr.bin.r4
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -18,7 +18,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl1271-fw-multirole-roc.bin
+LOCAL_MODULE := wl128x-fw-mr.bin.r5
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -26,7 +26,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-multirole-plt.bin
+LOCAL_MODULE := wl128x-fw-mr_plt.bin.r4
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -34,7 +34,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-multirole-roc.bin
+LOCAL_MODULE := wl128x-fw-mr_plt.bin.r5
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)

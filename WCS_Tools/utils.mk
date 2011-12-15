9957be95363c289cdef43abdc6c7efab9fac9689
Ido Reis <idor@ti.com>
2011-12-15 18:07:59 +0200

    utils: added some predefined utils
    Signed-off-by: Ido Reis <idor@ti.com>

--- WCS_Tools/utils.mk
+++ WCS_Tools/utils.mk
@@ -25,33 +25,35 @@
 ################################################################################
 
 MAKE_START_TIME:=$(shell echo $$(( $$(date +%s) )))
-GET_TIME = $$(date +%s)
+GET_TIME := $$(date +%s)
 
 # ------------------------------------------------------------------------------
 # utilities
 # ------------------------------------------------------------------------------
 
-PATCH=patch -N
-REM_PATCH=patch -R -N
-COPY=/bin/cp
-MOVE=/bin/mv
-DEL=/bin/rm
-LINK=/bin/ln
-SYMLINK=/bin/ln -s
-CHMOD=/bin/chmod
-MKDIR=/bin/mkdir
-RMDIR=/bin/rmdir
-ECHO=/bin/echo
-TAR=/bin/tar
-FIND=/usr/bin/find
-SED=/bin/sed
-MKFS_EXT4=/sbin/mkfs.ext4
+PATCH:=patch -N
+REM_PATCH:=patch -R -N
+COPY:=/bin/cp
+MOVE:=/bin/mv
+DEL:=/bin/rm
+LINK:=/bin/ln
+SYMLINK:=/bin/ln -s
+CHMOD:=/bin/chmod
+MKDIR:=/bin/mkdir
+RMDIR:=/bin/rmdir
+ECHO:=/bin/echo
+TAR:=/bin/tar
+FIND:=/usr/bin/find
+SED:=/bin/sed
+MKFS_EXT4:=/sbin/mkfs.ext4
+TOUCH:=/usr/bin/touch
+GIT:=/usr/bin/git
 
 
 # ------------------------------------------------------------------------------
 # functions
 # ------------------------------------------------------------------------------
 
-print = $(ECHO) -e "\033[7m$1\033[0m"
-echo-to-file = $(ECHO) $(1) > $(2)
+print := $(ECHO) -e "\033[7m$1\033[0m"
+echo-to-file := $(ECHO) $(1) > $(2)
 

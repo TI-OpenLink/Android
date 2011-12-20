b49b5115746ff39543b22f0f7637d610bad24af9
Ido Reis <idor@ti.com>
2011-12-20 15:27:46 +0200

    nlcp: added scripts for wlan features
    Signed-off-by: Ido Reis <idor@ti.com>

---
+++ WCS_Tools/patches/wlan/nlcp/android/packages/hardware/wlan/scripts/ap/Android.mk
@@ -0,0 +1,46 @@
+ifeq ($(BOARD_WLAN_DEVICE),wl12xx_mac80211)
+
+LOCAL_PATH := $(call my-dir)
+local_target_dir := $(TARGET_OUT)/data/misc/wifi
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ap_start.sh
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ap_stop.sh
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ap_cmd.sh
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := dhcpd.conf
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := udhcpd.leases
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+endif
---
+++ WCS_Tools/patches/wlan/nlcp/android/packages/hardware/wlan/scripts/sta/Android.mk
@@ -0,0 +1,30 @@
+ifeq ($(BOARD_WLAN_DEVICE),wl12xx_mac80211)
+
+LOCAL_PATH := $(call my-dir)
+local_target_dir := $(TARGET_OUT)/data/misc/wifi
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sta_start.sh
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sta_stop.sh
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sta_cmd.sh
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+endif

1e498d46253c84818ca307569d16b461d153f990
Ido Reis <idor@ti.com>
2011-12-25 18:29:20 +0200

    ps_lock: add chmod sequnce before echo
    Signed-off-by: Ido Reis <idor@ti.com>

--- WCS_Tools/binaries/root/ps_lock.sh
+++ WCS_Tools/binaries/root/ps_lock.sh
@@ -1,5 +1,10 @@
 #!/system/bin/sh
 
+chmod +w /sys/power/wake_lock
+chmod +w /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+chmod +w /sys/devices/system/cpu/cpu1/online
+chmod +w /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+
 echo manual_lock > /sys/power/wake_lock
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 1 > /sys/devices/system/cpu/cpu1/online

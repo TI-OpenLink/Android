652cd3d1d3653f415c11d7a6b9949472e729839d
Ido Reis <idor@ti.com>
2012-01-04 20:19:42 +0200

    added daily build script

---
+++ WCS_Tools/misc/scripts/daily-build_tag-and-update-manifest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+repo sync
+repo manifest -r -o ./workspace/ti-ol-manifest/TI-OpenLink-R5.00.y.xml
+pushd workspace/ti-ol-manifest/
+git commit TI-OpenLink-R5.00.y.xml -m $1
+git tag -f $1
+git remote set-url --push origin git@github.com:TI-OpenLink/ti-ol-manifest.git
+git push origin master
+popd
+

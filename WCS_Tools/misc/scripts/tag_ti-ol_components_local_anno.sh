19dc1f418980b75f92d0f59a44101adb1f0e1ed4
Ido Reis <idor@ti.com>
2012-05-23 14:45:04 +0300

    scripts: add local tag script

---
+++ WCS_Tools/misc/scripts/tag_ti-ol_components_local_anno.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export NEW_TAG_NAME=$1
+
+pushd .
+
+cd workspace/wlan
+repo forall -c 'echo "updating project ${REPO_PROJECT} with new tag ${NEW_TAG_NAME}" ; git tag -f -a ${NEW_TAG_NAME} -m ${NEW_TAG_NAME}'
+cd -
+
+cd workspace/mydroid
+repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink" ] ; then \
+	echo "updating project ${REPO_PROJECT} with new tag ${NEW_TAG_NAME}" ; \
+	git tag -f -a ${NEW_TAG_NAME} -m ${NEW_TAG_NAME} ; \
+fi'
+
+repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink-secured" ] ; then \
+	echo "updating project ${REPO_PROJECT} with new tag ${NEW_TAG_NAME}" ; \
+	git tag -f -a ${NEW_TAG_NAME} -m ${NEW_TAG_NAME} ; \
+fi'
+cd -
+
+popd

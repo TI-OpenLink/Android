dfea736e3df082037eb9bfb186c3e839c7d35e4e
Ido Reis <idor@ti.com>
2012-05-23 14:06:29 +0300

    wlan: remove default load_script.sh from install

--- WCS_Tools/cores_make/wlan.mk
+++ WCS_Tools/cores_make/wlan.mk
@@ -63,8 +63,6 @@ PROGRESS_WLAN_IBI_BRINGUP:=$(PROGRESS_DIR)/wlan.ibi.bringup
 WLAN_GIT_COMPAT_TREE:=$(WLAN_COMPAT_DIR)
 WLAN_GIT_TREE:=$(WLCORE_DIR)
 
-WLAN_DEFAULT_DRIVER_LOAD_SCRIPT:=load_wlcore_hdk_rld2_rdl7_siso40.sh
-
 ################################################################################
 # rules
 ################################################################################
@@ -200,7 +198,6 @@ wlan-make-private:
 
 wlan-install-private:
 	@$(ECHO) "wlan install..."
-	@cd $(MYFS_DATA_PATH)/misc/wifi ; $(LINK) -svf $(WLAN_DEFAULT_DRIVER_LOAD_SCRIPT) load_wlcore.sh
 	@$(ECHO) "...done"
 	
 wlan-clean-private:

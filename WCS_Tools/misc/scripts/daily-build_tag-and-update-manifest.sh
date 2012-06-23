65abd58797de95c698737a973c37d0b4a18189b4
Ido Reis <idor@ti.com>
2012-06-24 01:46:47 +0300

    update manifest script

--- WCS_Tools/misc/scripts/daily-build_tag-and-update-manifest.sh
+++ WCS_Tools/misc/scripts/daily-build_tag-and-update-manifest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 pushd ./workspace/wlan
-repo manifest -r -o ../ti-ol-manifest/ti-ol-driver-manifest.R5.yy.xml
+repo manifest -r -o ../ti-ol-manifest/ti-ol-driver-manifest.R8.yy.xml
 popd
 
 pushd ./workspace/mydroid
 repo manifest -r -o android-manifest.xml
 popd
 
-python ./WCS_Tools/misc/scripts/update_ti-ol_manifests.py ./workspace/mydroid/android-manifest.xml ./workspace/ti-ol-manifest/ti-ol-android-manifest.R5.yy.xml
+python ./WCS_Tools/misc/scripts/update_ti-ol_manifests.py ./workspace/mydroid/android-manifest.xml ./workspace/ti-ol-manifest/ti-ol-android-manifest.R8.yy.xml
 rm ./workspace/mydroid/android-manifest.xml
 
 pushd workspace/ti-ol-manifest/

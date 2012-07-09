9fbc848bcad3fa57f276d77d16000deb2f1d178a
Ido Reis <idor@ti.com>
2012-07-10 00:24:34 +0300

    wlan: add nl build scripts

---
+++ WCS_Tools/misc/scripts/create_nl_package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export ARCH=arm
+export CROSS_COMPILE=arm-none-linux-gnueabi-
+export DRIVER_DIR=`pwd`/workspace/wlan/wl18xx
+export NL_OUT_DIR=`pwd`/nl-artifacts
+
+mkdir -p ${NL_OUT_DIR}/boot
+
+cd $DRIVER_DIR
+make blaze_defconfig
+make -j12
+make uImage -j12
+make INSTALL_MOD_PATH=${NL_OUT_DIR} modules_install -j12
+cp arch/arm/boot/uImage ${NL_OUT_DIR}/boot
+cd -
+
+cd ${NL_OUT_DIR}
+tar cvf nl-artifcats.tar lib
+cd -
+

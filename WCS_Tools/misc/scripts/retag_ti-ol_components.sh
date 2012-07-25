3bfb889ec4578854886d3c0da99a2f75130a92c0
Ido Reis <idor@ti.com>
2012-07-25 21:05:02 +0300

    scripts: update tagging scripts

--- WCS_Tools/misc/scripts/retag_ti-ol_components.sh
+++ WCS_Tools/misc/scripts/retag_ti-ol_components.sh
@@ -6,19 +6,19 @@ export NEW_TAG_NAME=$2
 pushd .
 
 cd workspace/wlan
-repo forall -c 'git tag -f ${NEW_TAG_NAME} ${CURRENT_TAG_NAME} -m \"${NEW_TAG_NAME}\"'
+repo forall -c 'git tag -f ${NEW_TAG_NAME} ${CURRENT_TAG_NAME} '
 repo forall -c 'echo "updating project ${REPO_PROJECT} with new tag ${NEW_TAG_NAME}"'
-repo forall -c 'git push -f ${REPO_REMOTE} ${NEW_TAG_NAME}'
+repo forall -c 'git push ${REPO_REMOTE} ${NEW_TAG_NAME}'
 cd -
 
 cd workspace/mydroid
-repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink" ] ; then git tag -f ${NEW_TAG_NAME} ${CURRENT_TAG_NAME} -m \"${NEW_TAG_NAME}\" ; fi'
+repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink" ] ; then git tag -f ${NEW_TAG_NAME} ${CURRENT_TAG_NAME} ; fi'
 repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink" ] ; then echo "updating project ${REPO_PROJECT} with new tag ${NEW_TAG_NAME}" ; fi'
-repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink" ] ; then git push -f ${REPO_REMOTE} ${NEW_TAG_NAME} ; fi'
+repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink" ] ; then git push ${REPO_REMOTE} ${NEW_TAG_NAME} ; fi'
 
-repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink-secured" ] ; then git tag -f ${NEW_TAG_NAME} ${CURRENT_TAG_NAME} -m \"${NEW_TAG_NAME}\" ; fi'
+repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink-secured" ] ; then git tag -f ${NEW_TAG_NAME} ${CURRENT_TAG_NAME} ; fi'
 repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink-secured" ] ; then echo "updating project ${REPO_PROJECT} with new tag ${NEW_TAG_NAME}" ; fi'
-repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink-secured" ] ; then git push -f ${REPO_REMOTE} ${NEW_TAG_NAME} ; fi'
+repo forall -c 'if [ ${REPO_REMOTE} == "TI-OpenLink-secured" ] ; then git push ${REPO_REMOTE} ${NEW_TAG_NAME} ; fi'
 cd -
 
 popd
